8577a6b3e4fbafc8e545fe3e2dd540a2d4002c18
Brian Greenwald <brian.greenwald@liferay.com>
2015-10-19 14:30:41 -0500

    LGS-2315 Finished implementing CRUD operations on Issues and Articles

--- docroot/WEB-INF/src/com/liferay/training/newsletter/listeners/JournalArticleListener.java
+++ docroot/WEB-INF/src/com/liferay/training/newsletter/listeners/JournalArticleListener.java
@@ -1,3 +1,4 @@
+
 package com.liferay.training.newsletter.listeners;
 
 import com.liferay.portal.ModelListenerException;
@@ -16,32 +17,34 @@ import com.liferay.portal.service.ClassNameLocalServiceUtil;
 import com.liferay.portlet.dynamicdatamapping.model.DDMStructure;
 import com.liferay.portlet.dynamicdatamapping.service.DDMStructureLocalServiceUtil;
 import com.liferay.portlet.journal.model.JournalArticle;
+import com.liferay.training.newsletter.model.Article;
+import com.liferay.training.newsletter.model.Issue;
 import com.liferay.training.newsletter.service.ArticleLocalServiceUtil;
 import com.liferay.training.newsletter.service.IssueLocalServiceUtil;
 
-import java.util.Calendar;
 import java.util.Date;
 
 public class JournalArticleListener extends BaseModelListener<JournalArticle> {
 
 	@Override
-	public void onAfterCreate(JournalArticle article) 
+	public void onAfterCreate(JournalArticle journalArticle)
 		throws ModelListenerException {
-		
+
 		String structureName = StringPool.BLANK;
 		try {
-			structureName = getStructureName(article);
+			structureName = getStructureName(journalArticle);
 		}
 		catch (Exception e) {
 			_log.error(String.format(
 				"Failed to retrieve structure with id %s: %s",
-				article.getStructureId(), e));
+				journalArticle.getStructureId(), e));
 		}
 
 		try {
-			Document articleContent = SAXReaderUtil.read(article.getContent());
+			Document articleContent =
+				SAXReaderUtil.read(journalArticle.getContent());
 
-			String titleXML = article.getTitle();
+			String titleXML = journalArticle.getTitle();
 			Document titleDoc = SAXReaderUtil.read(titleXML);
 			Element root = titleDoc.getRootElement();
 			Element titleElement = root.element(TITLE);
@@ -50,13 +53,13 @@ public class JournalArticleListener extends BaseModelListener<JournalArticle> {
 			String issueNumnber = parseField(articleContent, ISSUE_NO);
 			int issueNo = Integer.parseInt(issueNumnber);
 
-			long journalArticleId = article.getPrimaryKey();
-			long groupId = article.getGroupId();
-			long companyId = article.getCompanyId();
-			long userId = article.getUserId();
-			String userName = article.getUserName();
+			long journalArticleId = journalArticle.getPrimaryKey();
+			long groupId = journalArticle.getGroupId();
+			long companyId = journalArticle.getCompanyId();
+			long userId = journalArticle.getUserId();
+			String userName = journalArticle.getUserName();
 
-			if (structureName.equals(NEWSLETTER_ISSUE)) {
+			if (structureName.equalsIgnoreCase(NEWSLETTER_ISSUE)) {
 				String description = parseField(articleContent, DESCRIPTION);
 
 				String date = parseField(articleContent, ISSUE_DATE);
@@ -65,7 +68,7 @@ public class JournalArticleListener extends BaseModelListener<JournalArticle> {
 
 				try {
 					IssueLocalServiceUtil.addIssue(
-						journalArticleId, groupId, companyId, userId, userName, 
+						journalArticleId, groupId, companyId, userId, userName,
 						issueNo, title, description, issueDate, byline);
 				}
 				catch (Exception e) {
@@ -73,15 +76,15 @@ public class JournalArticleListener extends BaseModelListener<JournalArticle> {
 						"Unable to add Issue entitled \"%s\"", title));
 				}
 			}
-			else if (structureName.equals(NEWSLETTER_ARTICLE)) {
+			else if (structureName.equalsIgnoreCase(NEWSLETTER_ARTICLE)) {
 				String author = parseField(articleContent, AUTHOR);
 				String orderString = parseField(articleContent, ORDER);
 				int order = Integer.valueOf(orderString);
 				String content = parseField(articleContent, CONTENT);
-				
+
 				try {
 					ArticleLocalServiceUtil.addArticle(
-						journalArticleId, groupId, companyId, userId, userName, 
+						journalArticleId, groupId, companyId, userId, userName,
 						issueNo, title, author, order, content);
 				}
 				catch (Exception e) {
@@ -90,27 +93,143 @@ public class JournalArticleListener extends BaseModelListener<JournalArticle> {
 				}
 			}
 
-		} catch (DocumentException e) {
+		}
+		catch (DocumentException e) {
 			_log.error(String.format(
 				"Unable to retrieve contents for JournalArticle with id %d",
-				article.getArticleId()));
+				journalArticle.getArticleId()));
 		}
 	}
-	
+
 	@Override
-	public void onAfterUpdate(JournalArticle article) 
+	public void onAfterUpdate(JournalArticle journalArticle)
+		throws ModelListenerException {
+
+		String structureName = StringPool.BLANK;
+		try {
+			structureName = getStructureName(journalArticle);
+		}
+		catch (Exception e) {
+			_log.error(String.format(
+				"Failed to retrieve structure with id %s: %s",
+				journalArticle.getStructureId(), e));
+		}
+
+		try {
+			Document articleContent =
+				SAXReaderUtil.read(journalArticle.getContent());
+
+			String titleXML = journalArticle.getTitle();
+			Document titleDoc = SAXReaderUtil.read(titleXML);
+			Element root = titleDoc.getRootElement();
+			Element titleElement = root.element(TITLE);
+			String title = titleElement.getText();
+
+			String issueNumnber = parseField(articleContent, ISSUE_NO);
+			int issueNo = Integer.parseInt(issueNumnber);
+
+			long journalArticleId = journalArticle.getPrimaryKey();
+			long groupId = journalArticle.getGroupId();
+			long companyId = journalArticle.getCompanyId();
+			long userId = journalArticle.getUserId();
+			String userName = journalArticle.getUserName();
+
+			if (structureName.equalsIgnoreCase(NEWSLETTER_ISSUE)) {
+				String description = parseField(articleContent, DESCRIPTION);
+
+				String date = parseField(articleContent, ISSUE_DATE);
+				Date issueDate = new Date(Long.valueOf(date));
+				String byline = parseField(articleContent, BYLINE);
+
+				try {
+					IssueLocalServiceUtil.updateIssue(
+						journalArticleId, groupId, companyId, userId, userName,
+						issueNo, title, description, issueDate, byline);
+				}
+				catch (Exception e) {
+					_log.error(String.format(
+						"Unable to add Issue entitled \"%s\"", title));
+				}
+			}
+			else if (structureName.equalsIgnoreCase(NEWSLETTER_ARTICLE)) {
+				String author = parseField(articleContent, AUTHOR);
+				String orderString = parseField(articleContent, ORDER);
+				int order = Integer.valueOf(orderString);
+				String content = parseField(articleContent, CONTENT);
+
+				try {
+					ArticleLocalServiceUtil.updateArticle(
+						journalArticleId, groupId, companyId, userId, userName,
+						issueNo, title, author, order, content);
+				}
+				catch (Exception e) {
+					_log.error(String.format(
+						"Unable to add Article entitled \"%s\"", title));
+				}
+			}
+
+		}
+		catch (DocumentException e) {
+			_log.error(String.format(
+				"Unable to retrieve contents for JournalArticle with id %d",
+				journalArticle.getArticleId()));
+		}
+
+	}
+
+	public void onAfterRemove(JournalArticle journalArticle) 
 		throws ModelListenerException {
 		
+		String structureName = StringPool.BLANK;
+		try {
+			structureName = getStructureName(journalArticle);
+			System.out.println(structureName);
+		}
+		catch (Exception e) {
+			_log.error(String.format(
+				"Failed to retrieve structure with id %s: %s",
+				journalArticle.getStructureId(), e));
+		}
 		
+		long journalArticleId = journalArticle.getPrimaryKey();
+		
+		if (structureName.equalsIgnoreCase(NEWSLETTER_ISSUE)) {
+			try {
+				Issue issue = 
+					IssueLocalServiceUtil.getIssueByJournalArticleId(
+						journalArticleId);
+				IssueLocalServiceUtil.deleteIssue(issue);
+				System.out.println("Issue should be deleted");
+			}
+			catch (Exception e) {
+				_log.error(String.format(
+					"Unable to delete Issue with journalArticleId %d", 
+					journalArticleId));
+			}
+		}
+		else if (structureName.equalsIgnoreCase(NEWSLETTER_ARTICLE)) {
+			try {
+				Article article = 
+					ArticleLocalServiceUtil.getArticleByJournalArticleId(
+						journalArticleId);
+				ArticleLocalServiceUtil.deleteArticle(article);
+				System.out.println("Article should be deleted");
+			}
+			catch (Exception e) {
+				_log.error(String.format(
+					"Unable to delete Article with journalArticle %d", 
+					journalArticleId));
+			}
+		}
 		
 	}
 
-	private String getStructureName(JournalArticle article) 
+	private String getStructureName(JournalArticle article)
 		throws PortalException, SystemException, DocumentException {
 
 		String structureKey = article.getStructureId();
 
-		DDMStructure structure = 
+		DDMStructure structure =
 			DDMStructureLocalServiceUtil.getStructure(
 				article.getGroupId(), CLASS_NAME_ID, structureKey);
 
@@ -133,11 +252,11 @@ public class JournalArticleListener extends BaseModelListener<JournalArticle> {
 		return field.getText();
 	}
 
-	private static  Log _log = LogFactoryUtil.getLog(
-		JournalArticleListener.class.getName());
+	private static Log _log =
+		LogFactoryUtil.getLog(JournalArticleListener.class.getName());
 
-	private static final long CLASS_NAME_ID 
-		= ClassNameLocalServiceUtil.getClassNameId(JournalArticle.class);
+	private static final long CLASS_NAME_ID =
+		ClassNameLocalServiceUtil.getClassNameId(JournalArticle.class);
 
 	private static String ISSUE_NO = "Issue_Number";
 	private static String TITLE = "Title";
